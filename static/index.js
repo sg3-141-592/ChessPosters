a6f9131b2fa1aa9f022d0f40c866dab382dd222a
sg3-141-592 <sean@gilligan.me>
2021-08-01 15:33:32 +0000

    Getting player games from Lichess working

---
+++ static/index.js
@@ -0,0 +1,30 @@
+async function getPlayerGames() {
+    var url = new URL("https://lichess.org/api/games/user/MrFoxGaming");
+    url.search = new URLSearchParams({max: 10});
+    const response = await fetch(url);
+    const reader = response.body
+        .pipeThrough(new TextDecoderStream())
+        .getReader();
+    while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+            break;
+        }
+        extractGameData(value);
+    }
+}
+
+// TODO: Refactor this in future to use a generic method instead of having
+// a seperate regex for each value extracted.
+const whiteNameRegEx = /\[White \"(.+)\"\]/;
+const blackNameRegEx = /\[White \"(.+)\"\]/;
+
+function extractGameData(data) {
+    var whiteName = whiteNameRegEx.exec(data)[1];
+    var blackName = blackNameRegEx.exec(data)[1];
+    $('#playerGames').append(
+        $('<div>').html(
+            whiteName + " vs " + blackName + "\n"
+        )
+    );
+}
