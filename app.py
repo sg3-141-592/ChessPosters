e57f2b9012bdd38b70970bc1a53c4e7c3fe7e6b3
sg3-141-592 <sean@gilligan.me>
2021-08-07 09:18:06 +0000

    Added initial render endpoint

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, send_from_directory
+import uuid
 
 app = Flask(__name__, static_url_path='')
 
+@app.route('/api/render', methods=['POST', 'GET'])
+def render():
+    filename = str(uuid.uuid4())
+    return filename, 200
+
 # Serve static content - Replace with proper static site
 # in production
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def serve_index():
     return serve_static_files("index.html")
 
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods=['GET'])
 def serve_static_files(path):
     return send_from_directory('static', path)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
