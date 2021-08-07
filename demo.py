98a369cdffbdb2c583b996d44160a91cc98d4d16
sg3-141-592 <sean@gilligan.me>
2021-08-07 15:01:53 +0000

    Messing with board rendering settings

--- demo.py
+++ demo.py
@@ -5,9 +5,21 @@ pgn = open("example.pgn")
 
 game = chess.pgn.read_game(pgn)
 
+boardColors = {
+    'square light': '#ffffff',
+    'square dark': '#a1a1a1'
+}
+
 board = game.board()
+counter = 1
 for move in game.mainline_moves():
     board.push(move)
+    if counter%2 == 0:
+        outFile = open("static/rendered/1/output{}.svg".format(int(counter/2)), "w")
+        outFile.write(chess.svg.board(board, size=600,
+            colors=boardColors, coordinates=False))
+        outFile.close()
+    counter += 1
 
 cairosvg.svg2png(bytestring=chess.svg.board(board, size=600),
     write_to="image.png")
